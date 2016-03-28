f62435fd5ec2003bd462c18c01d041a494a33a8b
ciecclm <clm@imnu.edu.cn>
2016-03-28 16:49:39 +0800

    Initial Commit

---
+++ testrevert/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testrevert
+//
+//  Created by admin on 16/3/28.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
