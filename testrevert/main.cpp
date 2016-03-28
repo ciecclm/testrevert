31df164752a6f6e52aeb2307cee8396d6d60d3c3
ciecclm <clm@imnu.edu.cn>
2016-03-28 17:02:38 +0800

    revert2

--- testrevert/main.cpp
+++ testrevert/main.cpp
@@ -11,6 +11,7 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+
     std::cout << "1111111111111\n";
     std::cout << "22222222222222\n";
     std::cout << "3333333333\n";
