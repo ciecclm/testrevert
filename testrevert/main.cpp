7170dca1ec64c200259a27606fe3ffc0f3632fa3
ciecclm <clm@imnu.edu.cn>
2016-03-28 16:53:58 +0800

    3333333

--- testrevert/main.cpp
+++ testrevert/main.cpp
@@ -13,5 +13,6 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     std::cout << "1111111111111\n";
     std::cout << "22222222222222\n";
+    std::cout << "3333333333\n";
     return 0;
 }
